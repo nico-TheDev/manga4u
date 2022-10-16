53ee458947763f15cd06cbc553f6f7ada4e03a60
Nico Ignacio <norbertoignaciojr@gmail.com>
2022-10-16 16:50:23 +0800

    feat: initial footer

---
+++ src/components/layout/Footer.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Footer() {
+  return (
+    <footer className='bg-secondary-dark p-8 text-center text-white'>
+      All Rights Reserved
+    </footer>
+  );
+}
--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 
+import Footer from '@/components/layout/Footer';
 import Nav from '@/components/layout/Nav';
 
 export default function Layout({ children }: { children: React.ReactNode }) {
@@ -7,6 +8,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
     <>
       <Nav />
       {children}
+      <Footer />
     </>
   );
 }
